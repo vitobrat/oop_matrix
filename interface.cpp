57684f10c80bedf834eefca90c2b272a5af88c82
Victor Bratko <bratko.viktor2004@gmail.com>
2023-10-10 23:20:56 +0300

    add full intaerface

--- interface.cpp
+++ interface.cpp
@@ -1,43 +1,64 @@
 #include "interface.h"
 
-const int button_size_x = 40;
-const int button_size_y = 110;
-const int delta = 10;
+using namespace std;
+
+const int window_size_x = 530;
+const int window_size_y = 300;
+const int delta = 20;
+const int button_size_x = window_size_x / 5;
+const int button_size_y = window_size_y / 10;
+const int text_size_x = window_size_x / 5;
+const int text_size_y = window_size_y / 10;
+const int edit_size_x = window_size_x / 15;
+const int edit_size_y = window_size_y / 10;
 
 Interface::Interface(QWidget *parent)
     : QWidget(parent)
 {
     setWindowTitle("Практическая работа №3");
-    setFixedSize(500, 450);
+    setFixedSize(window_size_x, window_size_y);
 
-    name = new QLabel("Matrix: ", this);
-    name->setGeometry(50, 20, 50, 20);
-    fractional = new QLabel(" / ", this);
-    fractional->setGeometry(140, 20, 10, 20);
-    fractional = new QLabel(" / ", this);
-    fractional->setGeometry(140, 50, 10, 20);
+    size_text = new QLabel("Размерность: ", this);
+    size_text->setGeometry(window_size_x*1/5, window_size_y*1/20, text_size_x, text_size_y);
+    input_matrix_text = new QLabel("Элемент [0][0]:", this);
+    input_matrix_text->setGeometry(window_size_x*1/5, window_size_y*4/20, text_size_x, text_size_y);
+    fractional_text = new QLabel(" / ", this);
+    fractional_text->setGeometry(window_size_x*2/5 + 2*delta, window_size_y*4/20, text_size_x, text_size_y);
+    output_text = new QLabel("0 0 0", this);
+    output_text->setGeometry(window_size_x*1/5,  window_size_y*5/20, window_size_x/5, window_size_y/5);//пока не знаю насколько большой ее нужно делать
 
-    num = new QLineEdit("0", this);
-    num->setGeometry(100, 20, 40, 25);
-    den = new QLineEdit("1", this);
-    den->setGeometry(150, 20, 40, 25);
+    matrix_size_edit = new QLineEdit("3", this);
+    matrix_size_edit->setGeometry(window_size_x*2/5, window_size_y*1/20, edit_size_x, edit_size_y);
+    num_edit = new QLineEdit("0", this);
+    num_edit->setGeometry(window_size_x*2/5, window_size_y*4/20, edit_size_x, edit_size_y);
+    den_edit = new QLineEdit("1", this);
+    den_edit->setGeometry(window_size_x*2/5 + 3*delta, window_size_y*4/20, edit_size_x, edit_size_y);
 
+    size_btn = new QPushButton("Создать", this);
+    size_btn->setGeometry(window_size_x - button_size_x - delta, window_size_y*1/20, button_size_x, button_size_y);
+    save_btn = new QPushButton("Сохранить", this);
+    save_btn->setGeometry(window_size_x - button_size_x - delta, window_size_y*4/20, button_size_x, button_size_y);
     det_btn = new QPushButton("Определитель", this);
-    det_btn->setGeometry(delta, 380, button_size_y, button_size_x);
+    det_btn->setGeometry(window_size_x*0/5 + delta, window_size_y - 1.5*button_size_y, button_size_x, button_size_y);
     transport_btn = new QPushButton("Транспонировать", this);
-    transport_btn->setGeometry(2*delta + button_size_y, 380, button_size_y, button_size_x);
+    transport_btn->setGeometry(window_size_x*1/5 + 2*delta, window_size_y - 1.5*button_size_y, button_size_x, button_size_y);
     rang_btn = new QPushButton("Ранг", this);
-    rang_btn->setGeometry(3*delta + 2*button_size_y, 380, button_size_y, button_size_x);
+    rang_btn->setGeometry(window_size_x*2/5 + 3*delta, window_size_y - 1.5*button_size_y, button_size_x, button_size_y);
     print_btn = new QPushButton("Вывод матрицы", this);
-    print_btn->setGeometry(4*delta + 3*button_size_y, 380, button_size_y, button_size_x);
+    print_btn->setGeometry(window_size_x*3/5 + 4*delta, window_size_y - 1.5*button_size_y, button_size_x, button_size_y);
 }
 
 Interface::~Interface()
 {
-    delete name;
-    delete fractional;
-    delete num;
-    delete den;
+    delete size_text;
+    delete input_matrix_text;
+    delete fractional_text;
+    delete output_text;
+    delete matrix_size_edit;
+    delete num_edit;
+    delete den_edit;
+    delete size_btn;
+    delete save_btn;
     delete det_btn;
     delete transport_btn;
     delete rang_btn;
--- interface.h
+++ interface.h
@@ -10,9 +10,9 @@ class Interface : public QWidget
 {
     Q_OBJECT
 
-    QLabel *name, *fractional, *output;
-    QLineEdit *num, *den;
-    QPushButton *creat_btn, *det_btn, *transport_btn, *rang_btn, *print_btn;
+    QLabel *size_text, *input_matrix_text, *fractional_text, *output_text;
+    QLineEdit *num_edit, *den_edit, *matrix_size_edit;
+    QPushButton *size_btn, *save_btn, *det_btn, *transport_btn, *rang_btn, *print_btn;
 
 public:
     Interface(QWidget *parent = nullptr);
